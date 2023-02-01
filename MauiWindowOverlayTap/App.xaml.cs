c8536c9a9fd240bae5a7c1fb225665cafaf9ea20
MartyIX <203266+MartyIX@users.noreply.github.com>
2023-02-01 14:04:49 +0100

    Empty MAUI project

---
+++ MauiWindowOverlayTap/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiWindowOverlayTap;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiWindowOverlayTap/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiWindowOverlayTap;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
